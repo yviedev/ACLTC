0e10f9bf63ef80c9fe439cf0e69bc50b3be7def8
yvonne ike <yviedev@gmail.com>
2016-11-16 15:42:22 -0500

    Update answer to Dan's last challenge

--- deliberate_practice/dan.rb
+++ deliberate_practice/dan.rb
@@ -35,3 +35,44 @@ remove_string(["hello ", "good bye", " good morning", "  good afternoon"])
 #takes an array of words and prints the words whose sum of letters, when converted to ASCII values, are odd.
 #["bumblebee", "sky", "fox", "jump", "leopard", "horrible", "cloudy"] => ["bumblebee", "sky", "fox", "leopard", "horrible"]
 
+def odd (input)
+	odd_ary = []
+	sum = 0
+	sum_ary = []
+	hash = Hash.new(0)
+	final_ary = []
+	
+	input.each do |word|
+		word.each_byte do |ascii|
+			sum += ascii
+		end
+		
+		hash[word] = sum
+		
+		sum_ary << sum
+		sum = 0
+	end
+	
+	#p sum_ary
+	#p hash
+	
+	sum_ary.each do |num|
+		if num % 2 != 0 
+			odd_ary << num
+		end
+	end
+	
+	odd_ary.each do |num|
+		hash.each do |key, value|
+			if num == value
+				final_ary << key
+			end
+		end
+	end 
+	
+	final_ary
+end
+
+
+odd(["bumblebee", "sky", "fox", "jump", "leopard", "horrible", "cloudy"]) 
+#=> ["bumblebee", "sky", "fox", "leopard", "horrible"])
