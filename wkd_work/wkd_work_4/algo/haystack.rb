119d48358c928b8f4e28c394337e7418f67d60f7
Yvonne Ike <yviedev@gmail.com>
2016-12-07 07:39:00 -0500

    Finish a few more problems.

--- wkd_work/wkd_work_4/algo/haystack.rb
+++ wkd_work/wkd_work_4/algo/haystack.rb
@@ -1,5 +1,7 @@
-# Find the needle by writing one line of code. 
+# Find the needle by writing one line of code.
 # As an example, if haystack = [:hay, :needle, :hay], you'd pull it out with:
 # haystack[1]
 
 haystack = {hay: [:hay, :hay, :hay, {hay: {hay: [:hay, {hay: [:hay, :hay, :needle]}, :hay, :hay, :hay]}}, :hay, :hay]}
+
+p haystack[:hay][3][:hay][:hay][1][:hay][2]
--- wkd_work/wkd_work_4/oop/calc_3.rb
+++ wkd_work/wkd_work_4/oop/calc_3.rb
@@ -1,10 +1,10 @@
 # In this example, we have THREE calculators, the two included in the previous
 # exercises as well as Whiz Bang Calculator that can even calculate the hypotenuse
-# of a triangle if given the length of the other two sides. Figure out how to 
+# of a triangle if given the length of the other two sides. Figure out how to
 # DRY up all the code below - there shouldn't be a single method duplicated between
 # any two classes.
 
-class SimpleCalculator
+module SimpleCalculator
 
   def add(first_number, second_number)
     first_number + second_number
@@ -26,21 +26,7 @@ end
 
 class FancyCalculator
 
-  def add(first_number, second_number)
-    first_number + second_number
-  end
-
-  def subtract(first_number, second_number)
-    first_number - second_number
-  end
-
-  def multiply(first_number, second_number)
-    first_number * second_number
-  end
-
-  def divide(first_number, second_number)
-    first_number / second_number
-  end
+  include SimpleCalculator
 
   def square_root(number)
     Math.sqrt(number)
@@ -48,32 +34,18 @@ class FancyCalculator
 
 end
 
-class WhizBangCalculator
+class WhizBangCalculator < FancyCalculator
 
-  def add(first_number, second_number)
-    first_number + second_number
-  end
-
-  def subtract(first_number, second_number)
-    first_number - second_number
-  end
-
-  def multiply(first_number, second_number)
-    first_number * second_number
-  end
-
-  def divide(first_number, second_number)
-    first_number / second_number
-  end
+  include SimpleCalculator
 
   def square_root(number)
-    Math.sqrt(number)
+    super
   end
 
   def hypotenuse(first_number, second_number)
     Math.hypot(first_number, second_number)
   end
-  
+
   def exponent(first_number, exponent_number)
     total = 1
     exponent_number.times { total = multiply(total,first_number) }
@@ -83,4 +55,8 @@ class WhizBangCalculator
 end
 
 # Copy your driver code from the previous exercise and more below:
+calc = WhizBangCalculator.new
 
+p calc.square_root(9)
+p calc.exponent(2, 3)
+p calc.add(2, 5)
